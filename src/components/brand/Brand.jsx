3a96e741295af75c18f06d8718e3c5fde8fceb44
Aba Nicaisse <abawandjovunicaise@gmail.com>
2024-03-29 00:52:55 +0300

    Implemented Brand section

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import "./brand.css";
+import { google, slack, atlassian, dropbox } from "./imports";
 
 const Brand = () => {
   return (
-    <div>
-      <h1>Brand</h1>
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src={google} alt="Google" />
+      </div>
+      <div>
+        <img src={slack} alt="Slack" />
+      </div>
+      <div>
+        <img src={atlassian} alt="Atlassian" />
+      </div>
+      <div>
+        <img src={dropbox} alt="DropBox" />
+      </div>
     </div>
   );
 };
