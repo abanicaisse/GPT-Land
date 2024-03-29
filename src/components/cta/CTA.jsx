d2229a124978e7aeb2e22f28486ecd3ab38732bc
Aba Nicaisse <abawandjovunicaise@gmail.com>
2024-03-29 23:53:22 +0300

    Completed the possibility and cta sections

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,8 +3,16 @@ import "./cta.css";
 
 const CTA = () => {
   return (
-    <div>
-      <h1>CTA</h1>
+    <div className="gpt3__cta section__padding">
+      <div className="gpt3__cta-content">
+        <p>Request Early Access</p>
+        <h3>Register today & start exploring the endless possibilities</h3>
+      </div>
+      <div className="gpt3__cta-button">
+        <button className="button" type="button">
+          Get Started
+        </button>
+      </div>
     </div>
   );
 };
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import "./possibility.css";
 
+import PossibilityImage from "../../assets/possibility.png";
+
 const Possibility = () => {
   return (
-    <div>
-      <h1>Possibility</h1>
+    <div className="gpt3__possibility section__padding" id="possibility">
+      <div className="gpt3__possibility-image">
+        <img src={PossibilityImage} alt="Possibility" />
+      </div>
+      <div className="gpt3__possibility-content">
+        <h4>Request early access to get Started</h4>
+        <h1 className="gradient__text">
+          The possibility are behond our imaginations
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa iste
+          blanditiis voluptate odit similique hic porro corrupti quisquam,
+          tempore accusamus!
+        </p>
+        <h4>Request early access to get Started</h4>
+      </div>
     </div>
   );
 };
