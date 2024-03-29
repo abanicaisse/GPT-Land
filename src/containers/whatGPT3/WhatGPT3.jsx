0b29ee77cefa0ff0fc90b19a9e159af8d143e5b1
Aba Nicaisse <abawandjovunicaise@gmail.com>
2024-03-29 18:38:31 +0300

    Completed the what is GPT3 section

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,10 +1,44 @@
 import React from "react";
 import "./whatGPT3.css";
+import { Feature } from "../../components";
 
 const WhatGPT3 = () => {
   return (
-    <div>
-      <h1>WhatGPT3</h1>
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature
+          title={"What is GPT3"}
+          text={
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus sunt, dolor accusantium expedita quae ab! adipisicing elit. Temporibus sunt, dolor accusantium expedita quae ab!"
+          }
+        />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>explore the libray</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature
+          title={"Chatbots"}
+          text={
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus sunt, dolor accusantium expedita quae ab!"
+          }
+        />
+        <Feature
+          title={"Knowledge based"}
+          text={
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus sunt, dolor accusantium expedita quae ab!"
+          }
+        />
+        <Feature
+          title={"Education"}
+          text={
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus sunt, dolor accusantium expedita quae ab!"
+          }
+        />
+      </div>
     </div>
   );
 };
