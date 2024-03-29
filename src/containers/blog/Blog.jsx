eb915a5bb90f352ae3b14c7476643d041e24b14a
Aba Nicaisse <abawandjovunicaise@gmail.com>
2024-03-30 00:40:07 +0300

    implemented the blog section

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,10 +1,48 @@
 import React from "react";
+import { Article } from "../../components";
+import { blog01, blog02, blog03, blog04, blog05 } from "./imports";
 import "./blog.css";
 
 const Blog = () => {
   return (
-    <div>
-      <h1>Blog</h1>
+    <div className="gpt3__blog section__padding" id="blog">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">
+          A lot is happening, we are blogging about it
+        </h1>
+      </div>
+
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container_groupA">
+          <Article
+            imageUrl={blog01}
+            date="March 27th, 2024"
+            title="GPT3 and OpenAI is the future, Let us explore how it is"
+          />
+        </div>
+        <div className="gpt3__blog-container_groupB">
+          <Article
+            imageUrl={blog02}
+            date="March 27th, 2024"
+            title="GPT3 and OpenAI is the future, Let us explore how it is"
+          />
+          <Article
+            imageUrl={blog03}
+            date="March 27th, 2024"
+            title="GPT3 and OpenAI is the future, Let us explore how it is"
+          />
+          <Article
+            imageUrl={blog04}
+            date="March 27th, 2024"
+            title="GPT3 and OpenAI is the future, Let us explore how it is"
+          />
+          <Article
+            imageUrl={blog05}
+            date="March 27th, 2024"
+            title="GPT3 and OpenAI is the future, Let us explore how it is"
+          />
+        </div>
+      </div>
     </div>
   );
 };
